6ef7685de9ac75137b546a9ae4e9f008e710daa2
Dayoung Kim <dayoungkim@Dayoungs-iMac.local>
2026-05-13 16:16:58 +0900

    docs: complete the project and update backend config

--- main.tf
+++ main.tf
@@ -1,6 +1,13 @@
 terraform {
   required_version = ">= 1.5.0"
 
+  backend "azurerm" {
+  resource_group_name  = "rg-tfstate"
+  storage_account_name = "tfstatexxxx"
+  container_name       = "tfstate"
+  key                  = "prod.tfstate"
+}
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
